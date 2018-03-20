29b2ded7d0fcea2a8cb53c243a5e8e2b075c5a28
Rohini Barla <rohini.kumar@gmail.com>
2018-03-20 13:13:02 +0530

    Initial Commit

---
+++ Telugulo/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Telugulo
+//
+//  Created by Rohini Kumar Barla on 20/03/18.
+//  Copyright © 2018 Rohini Kumar Barla. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
