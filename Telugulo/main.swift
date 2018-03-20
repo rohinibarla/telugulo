68c9f31ce7e50669d7eb2d3eeccc939d6c31748e
Rohini Barla <rohini.kumar@gmail.com>
2018-03-20 13:19:08 +0530

    First code in Telugu

--- Telugulo/main.swift
+++ Telugulo/main.swift
@@ -6,7 +6,54 @@
 //  Copyright © 2018 Rohini Kumar Barla. All rights reserved.
 //
 
-import Foundation
+//: Playground - noun: a place where people can play
 
-print("Hello, World!")
+import Cocoa
+
+
+func ఇదిఅయితె(_ cond: Bool, _ block: () -> Void) {
+    if cond {
+        block()
+    }
+}
+
+func అయినంతవరకు(_ cond: () -> Bool, _ block: () -> Void) {
+    while (cond() == true) {
+        block()
+    }
+}
+
+func వ్రాయు(_ string: String) {
+    print(string)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var 😄 = 5
+
+ఇదిఅయితె (😄 < 10) {
+    వ్రాయు("\(😄) నంబరు  10 నంబరు కన్నా చిన్నది")
+}
+
+వ్రాయు("మద్యలో నంబరులు: ")
+
+అయినంతవరకు ({😄 < 9}) {
+    😄 = 😄 + 1
+    వ్రాయు("-> \(😄)")
+}
+
+
+
 
